5c5daeabfdb7734621384811947e8f56e59682fc
Miguel Rentes <miguel.rentes@gmail.com>
2017-01-10 14:34:58 +0000

    Adds solution for Pointer challenge.

---
+++ introduction/pointer.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Miguel Rentes on 10/01/2017.
+//
+
+#include "introduction.h"
+
+void update(int *a, int *b) {
+    int val_a = *a;
+    int val_b = *b;
+    *a = val_a + val_b;
+    *b = val_a - val_b;
+    if (*b < 0)
+        *b = -(*b);
+}
+
+int main(void) {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return EXIT_SUCCESS;
+}
