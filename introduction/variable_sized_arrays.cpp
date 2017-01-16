5f0dc7a1e8e1618a5d7e7503ef8a2d50d42be962
Miguel Rentes <miguel.rentes@gmail.com>
2017-01-16 14:58:14 +0000

    Adds solution for Variable Sized Arrays challenge.

--- introduction/variable_sized_arrays.cpp
+++ introduction/variable_sized_arrays.cpp
@@ -7,37 +7,27 @@
 using namespace std;
 
 int main(void) {
-    unsigned int n, q;
-    string line;
+    unsigned int n, q, index_size, a, b;
 
-    getline(cin, line);
-    n = (unsigned int) stoi(line.substr(0, 1));
-    q = (unsigned int) stoi(line.substr(2, 1));
+    cin >> n >> q;
 
     unsigned int* array[n]; // array with n pointers to the array indexes
-    unsigned int queries[n*q]; // array with all queries
     unsigned int* array_indexes = nullptr;
 
     for (int i = 0; i < n; i++) {
-        std::getline(cin, line);
+        cin >> index_size;
 
-        unsigned int index_size = (unsigned int) stoi(line.substr(0, 1));
-        array_indexes = (unsigned int*) calloc(1, index_size);
+        array_indexes = new unsigned int[(const int) index_size];
 
         for (int j = 0; j < index_size; j++) {
-            array_indexes[j] = (unsigned int) stoi(line.substr(2*(j+1), 1));
+            cin >> array_indexes[j];
         }
         array[i] = array_indexes;
     }
 
     for (int i = 0; i < q; i++) {
-        std::getline(cin, line);
-        queries[2*i] = (unsigned int) stoi(line.substr(0, 1));
-        queries[2*i+1] = (unsigned int) stoi(line.substr(2, 1));
-    }
-
-    for (int i = 0; i < q; i++) {
-        cout << array[queries[2*i]][queries[2*i+1]] << endl;
+        cin >> a >> b;
+        cout << array[a][b] << endl;
     }
 
     return EXIT_SUCCESS;
