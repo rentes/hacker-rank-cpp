8e041e40841c2f7e50ca05b58cbc3dbb28872774
Miguel Rentes <miguel.rentes@gmail.com>
2017-01-21 19:10:24 +0000

    Starts solution for the Lower Bound-STL challenge.

--- STL/lower_bound-stl.cpp
+++ STL/lower_bound-stl.cpp
@@ -7,7 +7,36 @@
 int main(void) {
     unsigned int number_elements;
     unsigned int number_queries;
+    unsigned int element;
+    bool present = false;
+    unsigned int index;
     cin >> number_elements;
+    vector <unsigned int> elements;
+    for (int i = 0; i < number_elements; i++) {
+        if (cin >> element) {
+            elements.push_back(element);
+        }
+    }
+    cin >> number_queries;
+    std::vector<unsigned int>::iterator low;
+    for (int i = 0; i < number_queries; i++) {
+        cin >> element;
+        for (int j = 0; j < number_elements; j++) {
+            if (elements.at(j) == element) {
+                present = true;
+                index = j;
+                break;
+            } else {
+                low = lower_bound(elements.begin(), elements.end(), element);
+            }
+        }
+        if (present) {
+            cout << "Yes " << index+1 << endl;
+        } else {
+            cout << "No " << low - elements.begin()+1 << endl;
+        }
+        present = false;
+    }
 
     return EXIT_SUCCESS;
 }
