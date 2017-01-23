462cc5f756edb5c7a2a9ee5d312705c264098697
Miguel Rentes <miguel.rentes@gmail.com>
2017-01-23 14:49:26 +0000

    Starts solution for Print Pretty challenge.

--- STL/print_pretty.cpp
+++ STL/print_pretty.cpp
@@ -5,7 +5,17 @@
 #include "STL.h"
 
 int main(void) {
-
+    int T; cin >> T;
+    cout << setiosflags(ios::uppercase);
+    cout << setw(0xf) << internal;
+    while(T--) {
+        double A;
+        cin >> A;
+        double B;
+        cin >> B;
+        double C;
+        cin >> C;
+    }
     return EXIT_SUCCESS;
 }
 
