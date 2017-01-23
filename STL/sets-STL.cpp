9253ccaa3fac7b9b4719594a18bef28089b21fd7
Miguel Rentes <miguel.rentes@gmail.com>
2017-01-23 13:50:47 +0000

    Refactors solution for Sets-STL challenge.

--- STL/sets-STL.cpp
+++ STL/sets-STL.cpp
@@ -13,19 +13,23 @@ int main(void) {
     for (int i = 0; i < number_queries; i++) {
         cin >> operation_type >> value;
         switch (operation_type) {
-            case 1:
+            case 1: {
                 st.insert(value);
                 break;
-            case 2:
+            }
+            case 2: {
                 st.erase(value);
                 break;
-            case 3:
+            }
+            case 3: {
                 set<unsigned int>::iterator itr = st.find(value);
                 if (itr != st.end()) {
                     cout << "Yes" << endl;
                 } else {
                     cout << "No" << endl;
                 }
+            }
+            default: break;
         }
     }
     return EXIT_SUCCESS;
