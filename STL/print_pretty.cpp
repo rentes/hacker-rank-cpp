4780b1a192be15df0c2e21d43721e694effa4121
Miguel Rentes <miguel.rentes@gmail.com>
2017-01-24 12:47:21 +0000

    Adds pretty printing B solution for Print Pretty challenge.

--- STL/print_pretty.cpp
+++ STL/print_pretty.cpp
@@ -17,6 +17,14 @@ int main(void) {
         cin >> C;
         // pretty printing A
         cout << "0x" << hex << (int) A << endl;
+        // pretty printing B
+        // width is 15 minus the number of digits of B
+        // because 1 character is for the '.', and 2 for the decimal places
+        // so the width is 15 - 1 - 2 = 12 minus the number of digits of B
+        cout << fixed << setprecision(2) << setfill('_') << setw(12-((int) std::log10(B)+1));
+        if (B > 0)
+             cout << "+" << B << endl;
+        else cout << "-" << B << endl;
     }
 
     return EXIT_SUCCESS;
