96842c0e742440abce8b6034fa5b04eafde1fb6f
Miguel Rentes <miguel.rentes@gmail.com>
2017-01-30 16:24:55 +0000

    Starts solution for Deque-STL challenge.

--- STL/deque-STL.cpp
+++ STL/deque-STL.cpp
@@ -4,7 +4,22 @@
 
 #include "STL.h"
 
-int main(void) {
+void printKMax(int arr[], int n, int k){
 
+}
+
+int main(void) {
+    int t;
+    cin >> t;
+    while (t > 0) {
+        int n, k;
+        cin >> n >> k;
+        int i;
+        int arr[n];
+        for (i = 0; i < n; i++)
+            cin >> arr[i];
+        printKMax(arr, n, k);
+        t--;
+    }
     return EXIT_SUCCESS;
 }
