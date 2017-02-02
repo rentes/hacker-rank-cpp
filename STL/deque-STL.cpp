3b550c4f06581b7a21fde683dadf44fca27409d0
Miguel Rentes <miguel.rentes@gmail.com>
2017-02-02 23:06:08 +0000

    Starts solution for the Deque-STL challenge.

--- STL/deque-STL.cpp
+++ STL/deque-STL.cpp
@@ -7,21 +7,22 @@
 void printKMax(int arr[], int n, int k) {
     deque<int> mydeque;
     int greatest = 0;
-    // creating the deque from the array k-elements
-    for (int i = 0; i < n-k+1; i++) {
-        for (int j = i; j < i+k; j++) {
-            mydeque.push_back(arr[j]);
+    int pop = 0;
+
+    for (int i = 0; i < n;) {
+        mydeque.push_back(arr[i]);
+        int value_back = mydeque.back();
+        if (value_back > greatest)
+            greatest = value_back;
+        mydeque.pop_back();
+        pop++;
+        if (pop % k == 0) {
+            cout << greatest << " ";
+            greatest = 0;
+            i=i-k+2;
+            pop = 0;
         }
-        // after each evaluation we pop the front value
-        for (int j = 0; j < k; j++) {
-            int value_front;
-            value_front = mydeque.front();
-            mydeque.pop_front();
-            if (value_front > greatest)
-                greatest = value_front;
-        }
-        cout << greatest << " ";
-        greatest = 0;
+        else i++;
     }
     cout << endl;
 }
