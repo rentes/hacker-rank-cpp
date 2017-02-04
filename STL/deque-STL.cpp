532e0f638ab8d207361878c6a356ec696d0953b6
Miguel Rentes <miguel.rentes@gmail.com>
2017-02-04 15:14:54 +0000

    Starts solution for the Deque-STL challenge.

--- STL/deque-STL.cpp
+++ STL/deque-STL.cpp
@@ -6,9 +6,21 @@
 
 void printKMax(int arr[], int n, int k) {
     deque<int> mydeque;
+    std::deque<int>::iterator it = mydeque.begin();
     int greatest = 0;
+    int value = 0;
+    int index = k;
     // for each group of k-elements:
     // push it to the deque
+    for (int i = 0; i < k; i++) {
+        mydeque.push_front(arr[i]);
+        value = mydeque.front();
+        if (value > greatest)
+            greatest = value;
+    }
+
+    while (it != mydeque.end())
+        std::cout << *it++ << ' ';
     // iterate over it and find the greatest value
     // on the next iterations, push only one more value
     // and iterate again over the first k-elements
