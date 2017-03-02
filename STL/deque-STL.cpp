adbd06f884aad9b9186d958d75fa03248724a348
Miguel Rentes <miguel.rentes@gmail.com>
2017-03-02 14:53:35 +0000

    Starts solution for the Deque-STL challenge.

--- STL/deque-STL.cpp
+++ STL/deque-STL.cpp
@@ -5,17 +5,6 @@
 #include "STL.h"
 
 void printKMax(int arr[], int n, int k) {
-    /**
-     * Sample input
-     * 2
-     * 5 2
-     * 3 4 6 3 4
-     * 7 4
-     * 3 4 5 8 1 4 10
-     * Sample output
-     * 4 6 6 4
-     * 8 8 8 10
-     */
     deque<int> mydeque;
     int greatest = 0, value = 0, index = 0, iteration = 0;
     /*
