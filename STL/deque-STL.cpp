8b1c288b6ca681fff35417f077b511549bb0800e
Miguel Rentes <miguel.rentes@gmail.com>
2017-03-03 23:08:44 +0000

    Adds solution for the Deque-STL challenge.

--- STL/deque-STL.cpp
+++ STL/deque-STL.cpp
@@ -6,30 +6,15 @@
 
 void printKMax(int arr[], int n, int k) {
     deque<int> mydeque;
-    int greatest = 0, value = 0, index = 0, iteration = 0;
-    // pushing all the greatest values on each k-elements to the front of the deque
     for (int i = 0; i < n; i++) {
-        if (index < k) {
-            value = arr[i];
-            if (value > greatest)
-                greatest = value;
-            if (index == k - 1) {
-                mydeque.push_front(greatest);
-                greatest = 0;
-                index = 0;
-                iteration++;
-                i = iteration-1;
-            } else {
-                index++;
-            }
-        }
+        for (; !mydeque.empty() && arr[i] > mydeque.back();)
+            mydeque.pop_back();
+        mydeque.push_back(arr[i]);
+        if (i >= k && mydeque.front() == arr[i-k])
+            mydeque.pop_front();
+        if (i >= k-1)
+            printf (i < n-1 ? "%d " : "%d\n", mydeque.front());
     }
-    // pop-ing the greatest values from the back of the deque
-    while(mydeque.size() > 0) {
-        printf("%d ", mydeque.back());
-        mydeque.pop_back();
-    }
-    printf("\n");
 }
 
 int main(void) {
