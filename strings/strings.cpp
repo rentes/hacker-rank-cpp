75804c2b1b14cd4523ac17b4d627d82949892c4d
Miguel Rentes <miguel.rentes@gmail.com>
2017-03-08 23:13:19 +0000

    Adds solution for the Strings challenge.

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -2,3 +2,20 @@
 // Created by Miguel Rentes on 08/03/17.
 //
 
+#include "strings.h"
+
+int main() {
+    string a, b;
+    char swap;
+    cin >> a;
+    cin >> b;
+
+    cout << a.size() << " " << b.size() << endl;
+    cout << a + b << endl;
+    swap = a[0];
+    a[0] = b[0];
+    b[0] = swap;
+    cout << a << " " << b << endl;
+
+    return EXIT_SUCCESS;
+}
--- strings/strings.h
+++ strings/strings.h
@@ -4,5 +4,7 @@
 
 #ifndef HACKER_RANK_CPP_STRINGS_H
 #define HACKER_RANK_CPP_STRINGS_H
-
+#include <iostream>
+#include <string>
+using namespace std;
 #endif //HACKER_RANK_CPP_STRINGS_H
