89218febc4d9c86fbbc7844f3a11ee3ccfd318f7
Miguel Rentes <miguel.rentes@gmail.com>
2017-03-11 22:48:39 +0000

    Starts solution for the Attribute Parser challenge.

--- strings/attribute_parser.cpp
+++ strings/attribute_parser.cpp
@@ -2,3 +2,9 @@
 // Created by Miguel Rentes on 11/03/17.
 //
 
+#include "strings.h"
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return EXIT_SUCCESS;
+}
