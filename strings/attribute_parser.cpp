51a88d684938f8b0a24cb9ae0e13ba0730179029
Miguel Rentes <miguel.rentes@gmail.com>
2017-03-15 22:52:11 +0000

    Starts solution for the Attribute Parser challenge.

--- strings/attribute_parser.cpp
+++ strings/attribute_parser.cpp
@@ -4,6 +4,22 @@
 
 #include "strings.h"
 
+/**
+ * Sample input
+ * 4 3
+ * <tag1 value = "HelloWorld">
+ * <tag2 name = "Name1">
+ * </tag2>
+ * </tag1>
+ * tag1.tag2~name
+ * tag1~name
+ * tag1~value
+ *
+ * Sample output
+ * Name1
+ * Not Found!
+ * HelloWorld
+ */
 int main() {
     unsigned int n, q;
     cin >> n >> q;
