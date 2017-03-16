3232fd3b76234061b4f96063e4b1fd484ae00062
Miguel Rentes <miguel.rentes@gmail.com>
2017-03-16 22:30:03 +0000

    Starts solution for the Attribute Parser challenge.

--- strings/attribute_parser.cpp
+++ strings/attribute_parser.cpp
@@ -4,22 +4,6 @@
 
 #include "strings.h"
 
-/**
- * Sample input
- * 4 3
- * <tag1 value = "HelloWorld">
- * <tag2 name = "Name1">
- * </tag2>
- * </tag1>
- * tag1.tag2~name
- * tag1~name
- * tag1~value
- *
- * Sample output
- * Name1
- * Not Found!
- * HelloWorld
- */
 int main(void) {
     unsigned int n, q;
     cin >> n >> q;
@@ -30,12 +14,12 @@ int main(void) {
     cin.ignore();
 
     for(int i = 0; i < n; i++) {
-        getline(cin,temp);
+        getline(cin, temp);
         hrml.push_back(temp);
     }
 
     for(int i = 0; i < q; i++) {
-        getline(cin,temp);
+        getline(cin, temp);
         quer.push_back(temp);
     }
 
@@ -44,34 +28,33 @@ int main(void) {
 
     for(int i = 0; i < n; i++) {
         temp=hrml[i];
-        temp.erase(remove(temp.begin(), temp.end(), '\"' ),temp.end());
-        temp.erase(remove(temp.begin(), temp.end(), '>' ),temp.end());
+        temp.erase(remove(temp.begin(), temp.end(), '\"'), temp.end());
+        temp.erase(remove(temp.begin(), temp.end(), '>'), temp.end());
 
-        if(temp.substr(0,2)=="</") {
+        if(temp.substr(0,2) == "</") {
             tag.pop_back();
         }
         else {
             stringstream ss;
             ss.str("");
-            ss<<temp;
-            string t1,p1,v1;
+            ss << temp;
+            string t1, p1, v1;
             char ch;
-            ss>>ch>>t1>>p1>>ch>>v1;
-            string temp1="";
+            ss >> ch >> t1 >> p1 >> ch >> v1;
+            string temp1 = "";
             if(tag.size() > 0) {
-                temp1=*tag.rbegin();
-                temp1=temp1+"."+t1;
+                temp1 =* tag.rbegin();
+                temp1 = temp1 + "." + t1;
             }
             else
-                temp1=t1;
+                temp1 = t1;
             tag.push_back(temp1);
-            m[*tag.rbegin()+"~"+p1]=v1;
+            m[*tag.rbegin() + "~" + p1] = v1;
             while(ss) {
-                ss>>p1>>ch>>v1;
-                m[*tag.rbegin()+"~"+p1]=v1;
+                ss >> p1 >> ch >> v1;
+                m[*tag.rbegin() + "~" + p1] = v1;
             }
         }
-
     }
 
     for(int i = 0; i < q; i++) {
@@ -80,6 +63,5 @@ int main(void) {
         else
             cout<<m[quer[i]]<<endl;
     }
-
     return EXIT_SUCCESS;
 }
