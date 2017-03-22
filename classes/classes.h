e5f16f695f62ff846b990061fbe944160ea6a813
Miguel Rentes <miguel.rentes@gmail.com>
2017-03-22 12:19:33 +0000

    Adds solution for the Structs challenge.

---
+++ classes/classes.h
@@ -0,0 +1,14 @@
+//
+// Created by 0930 on 22/03/2017.
+//
+
+#ifndef HACKER_RANK_CPP_CLASSES_H
+#define HACKER_RANK_CPP_CLASSES_H
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+#endif //HACKER_RANK_CPP_CLASSES_H
